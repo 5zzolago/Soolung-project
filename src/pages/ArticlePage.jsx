a40407e01052574bee31cd2408d4358c24ef20d1
Yuan Kim <mini5167@naver.com>
2022-12-22 19:55:49 +0900

    test입니다. 무시하세요

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const ArticlePage = () => {
-  return <div>ArticlePage</div>;
+  return (
+    <>
+      <div>ArticlePage</div>
+      <div>"test입니다. 무시하세요"</div>
+    </>
+  );
 };
 
 export default ArticlePage;
