4a3cc3544b874e34c66f6f2a74139935e47394f2
LeeYujeong <yujlee242@gmail.com>
2022-12-22 17:56:10 +0900

    feat. 헤더 컴포넌트 UI 추가 및 네비게이션 라우터 연결

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,7 +1,72 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Box } from "@mui/system";
+import styled from "styled-components";
+import logo from "../assets/logo.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <HeaderWrap>
+      <Container
+        maxWidth="lg"
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box>
+          <Link to={"/"}>
+            <Logo>
+              <img src={logo} alt="로고" height="32px" />
+            </Logo>
+          </Link>
+        </Box>
+        <Box>
+          <Nav>
+            <ul>
+              <li>
+                <Link to={"/article"}>Article</Link>
+              </li>
+              <li>
+                <Link to={"/board"}>Board</Link>
+              </li>
+            </ul>
+          </Nav>
+        </Box>
+      </Container>
+    </HeaderWrap>
+  );
 };
 
 export default Header;
+
+// 헤더 스타일 컴포넌트
+const HeaderWrap = styled.header`
+  width: 100%;
+  height: 80px;
+  border-bottom: 1px solid #ccc;
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.h1`
+  padding-top: 8px;
+`;
+
+const Nav = styled.nav`
+  ul {
+    display: flex;
+
+    li {
+      width: 48px;
+      &:first-child {
+        margin-right: 40px;
+      }
+
+      &:hover {
+        font-weight: 700;
+      }
+    }
+  }
+`;
