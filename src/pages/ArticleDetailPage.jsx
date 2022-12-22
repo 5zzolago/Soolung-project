614e765e30acde033c1b52b3ae53288d67233578
vpvm96 <vpvm96@gmail.com>
2022-12-22 21:31:07 +0900

    feat. add ArticleDetailPage

--- src/pages/ArticleDetailPage.jsx
+++ src/pages/ArticleDetailPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import ArticleDetailHeader from "../components/article/ArticleDetailHeader";
+import ArticleDetailCommentWrite from "../components/article/ArticleDetailCommentWrite";
+import styled from "styled-components";
 
 const ArticleDetailPage = () => {
-  return <div>ArticleDetailPage</div>;
+  return (
+    <ArticleDetailWrap>
+      <ArticleDetailHeader />
+      <ArticleDetailCommentWrite />
+    </ArticleDetailWrap>
+  );
 };
+
+const ArticleDetailWrap = styled.div`
+  width: 100%;
+  height: 90vh;
+`;
 
 export default ArticleDetailPage;
