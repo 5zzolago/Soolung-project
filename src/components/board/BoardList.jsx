9bae3bcbd9fef3f77181f3489049640eed7b1287
LeeYujeong <yujlee242@gmail.com>
2022-12-24 12:04:27 +0900

    fix. 게시글 없을 경우에 대한 처리, 관련 함수 분리

--- src/components/board/BoardList.jsx
+++ src/components/board/BoardList.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import { useLocation, Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import BoardItem from "./BoardItem";
-import Button from "../button/Button";
-import { Typography, Pagination } from "@mui/material";
 import styled from "styled-components";
+import { Typography, Pagination } from "@mui/material";
+import Button from "../button/Button";
+import BoardItem from "./BoardItem";
+import NoContent from "../error/NoContent";
 import { getBoard, getAllBoard } from "../../store/modules/boardSlice";
 
 const BoardList = () => {
@@ -23,12 +24,27 @@ const BoardList = () => {
   useEffect(() => {
     // currentLocation === "" 인 경우 (falsy한 값) getAllBoard() 실행
     !currentLocation ? dispatch(getBoard()) : dispatch(getAllBoard());
-  }, [dispatch]);
+  }, [currentLocation, dispatch]);
 
   // 페이지 이동 핸들러
   const handlePageChange = (event) => {
     const currentPageIndex = Number(event.target.outerText);
     setCurrentPage(currentPageIndex);
+  };
+
+  // 게시글 리스트 렌더링 핸들러
+  const handleBoardList = () => {
+    if (board.length === 0) {
+      return <NoContent />;
+    }
+    // currentLocation === "" 인 경우 최신순 5개 일괄 출력 / 아닐 경우 페이지네이션 적용
+    if (!currentLocation) {
+      return board.map((item) => <BoardItem key={item.id} item={item} />);
+    } else {
+      return board
+        .slice(5 * (currentPage - 1), 5 * (currentPage - 1) + 5)
+        .map((item) => <BoardItem key={item.id} item={item} />);
+    }
   };
 
   return (
@@ -39,31 +55,28 @@ const BoardList = () => {
       >
         <Link to={"/board"}>Board</Link>
       </Typography>
-      <div>
-        {!currentLocation &&
-          board.map((item) => <BoardItem key={item.id} item={item} />)}
-        {currentLocation &&
-          board
-            .slice(5 * (currentPage - 1), 5 * (currentPage - 1) + 5)
-            .map((item) => <BoardItem key={item.id} item={item} />)}
-      </div>
-      <ButtonWrap page={!currentLocation ? "main" : "board"}>
-        <Button
-          size={!currentLocation ? "primary" : "secondary"}
-          type={!currentLocation ? "more" : "editor"}
-          text={!currentLocation ? "더보기" : "글쓰기"}
-        />
-      </ButtonWrap>
-      <PaginationWrap>
-        {currentLocation && (
-          <Pagination
-            page={currentPage}
-            count={LAST_PAGE}
-            defaultPage={1}
-            onChange={handlePageChange}
+      <div>{handleBoardList()}</div>
+      {board.length !== 0 && (
+        <ButtonWrap page={!currentLocation ? "main" : "board"}>
+          <Button
+            size={!currentLocation ? "primary" : "secondary"}
+            type={!currentLocation ? "more" : "editor"}
+            text={!currentLocation ? "더보기" : "글쓰기"}
           />
-        )}
-      </PaginationWrap>
+        </ButtonWrap>
+      )}
+      {board.length !== 0 && (
+        <PaginationWrap>
+          {currentLocation && (
+            <Pagination
+              page={currentPage}
+              count={LAST_PAGE}
+              defaultPage={1}
+              onChange={handlePageChange}
+            />
+          )}
+        </PaginationWrap>
+      )}
     </Section>
   );
 };
