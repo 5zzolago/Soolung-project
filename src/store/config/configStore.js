44b58158ef2dacd10274ec538a0c1fe38602658d
vpvm96 <vpvm96@gmail.com>
2022-12-24 15:15:10 +0900

    feat. articlePage redux

--- src/store/config/configStore.js
+++ src/store/config/configStore.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import articleSlice from "../modules/articleSlice";
+import article from "../modules/articleSlice";
 import boardSlice from "../modules/boardSlice";
 import articleComment from "../modules/articleCommentSlice";
 
 const store = configureStore({
   reducer: {
-    articleSlice: articleSlice,
+    article: article,
     boardSlice: boardSlice,
     articleComment: articleComment,
   },
