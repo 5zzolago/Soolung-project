794a448f1ebeb45aa32efb03ee08c79a103dba90
LeeYujeong <yujlee242@gmail.com>
2022-12-26 22:41:01 +0900

    fix. 게시글 상세페이지 UI 수정 및 수정, 삭제 아이콘 추가

--- src/pages/BoardDetailPage.jsx
+++ src/pages/BoardDetailPage.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-
-import BoardDetailWrite from "../components/board/BoardDetailWrite";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faXmark } from "@fortawesome/free-solid-svg-icons";
+import BoardDetailCommentWrite from "../components/board/BoardDetailCommentWrite";
 
 const BoardDetailPage = () => {
   const [postData, setPostData] = useState([]);
@@ -17,16 +18,26 @@ const BoardDetailPage = () => {
 
   return (
     <BoardDetailWrap>
-      <BoardDetailTitleContainer>
-        <BoardDetailTitleName>{post?.title}</BoardDetailTitleName>
-        <BoardDetailNameAt>
-          <BoardDetailNickName>{post?.username}</BoardDetailNickName>
-          <BoardDetailCreateAt>{post?.createdDate}</BoardDetailCreateAt>
-        </BoardDetailNameAt>
-      </BoardDetailTitleContainer>
+      <BoardDetailHeader>
+        <BoardDetailTitleContainer>
+          <BoardDetailTitleName>{post?.title}</BoardDetailTitleName>
+          <BoardDetailNameAt>
+            <BoardDetailNickName>by. {post?.username}</BoardDetailNickName>
+            <BoardDetailCreateAt>{post?.createdDate}</BoardDetailCreateAt>
+          </BoardDetailNameAt>
+        </BoardDetailTitleContainer>
+        <BoardEdtingBtnContainer>
+          <BoardIconWrapper>
+            <FontAwesomeIcon icon={faPenToSquare} color={"#aaa"} />
+          </BoardIconWrapper>
+          <BoardIconWrapper>
+            <FontAwesomeIcon icon={faXmark} color={"#aaa"} />
+          </BoardIconWrapper>
+        </BoardEdtingBtnContainer>
+      </BoardDetailHeader>
       <BoardDetailPostingContainer>
         <BoardDetailImage>
-          <img src={post?.img} alt="wine" height="500px" />
+          <img src={post?.img} alt="이미지" />
         </BoardDetailImage>
         <BoardDetailDesc>
           If you're the kind of person who stares at the endless shelves of wine
@@ -35,7 +46,7 @@ const BoardDetailPage = () => {
           you, we've compiled a list of easy-drinking wines to help you identify
           and build your wine palate.
         </BoardDetailDesc>
-        <BoardDetailWrite />
+        <BoardDetailCommentWrite />
       </BoardDetailPostingContainer>
     </BoardDetailWrap>
   );
@@ -48,30 +59,31 @@ const BoardDetailTitleContainer = styled.div`
   width: 100%;
   height: 100px;
   margin-bottom: 10px;
-  position: relative;
 `;
 
 const BoardDetailTitleName = styled.h1`
-  /* position: relative; */
-  font-weight: bold;
-  text-align: left;
-  margin-top: 4rem;
+  color: #101010;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 const BoardDetailNameAt = styled.div`
-  text-align: center;
+  display: flex;
+  justify-content: flex-start;
   margin-top: 10px;
+  color: #555;
 `;
 
 const BoardDetailNickName = styled.p`
-  position: absolute;
-  font-weight: bold;
+  width: 140px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 const BoardDetailCreateAt = styled.p`
-  position: absolute;
-  font-weight: lighter;
-  margin-left: 100px;
+  margin-left: 3rem;
 `;
 
 const BoardDetailPostingContainer = styled.div`
@@ -79,18 +91,43 @@ const BoardDetailPostingContainer = styled.div`
 `;
 
 const BoardDetailImage = styled.div`
-  margin: 0 auto;
-  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin: 3rem 0 1rem;
+
   img {
-    display: inline-block;
+    display: block;
   }
 `;
 
 const BoardDetailDesc = styled.p`
-  font-weight: lighter;
+  color: #555;
   font-size: 1.1rem;
-  margin-top: 4rem;
-  margin-bottom: 4rem;
+  margin: 4rem 0 8rem;
+`;
+
+const BoardDetailHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100px;
+  margin-top: 5rem;
+`;
+
+const BoardEdtingBtnContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const BoardIconWrapper = styled.div`
+  font-size: 28px;
+  cursor: pointer;
+
+  &:first-child {
+    margin-right: 1.6rem;
+  }
 `;
 
 export default BoardDetailPage;
